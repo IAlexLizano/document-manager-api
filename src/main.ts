d3c3bdd180ca16fe37e396304293a9a26979b625
Alex <alexsanty10lm@gmail.com>
2025-11-04 20:14:49 -0500

    First commit

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Configuración de Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Document Manager API')
+    .setDescription('API para la gestión de documentos')
+    .setVersion('1.0')
+    .addTag('documents')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
